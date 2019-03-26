3432cf07a2eb2d4246d9a8e0c4184871d668205d
HJBowers <hjbowers@gmail.com>
2019-03-26 02:03:48 +0000

    Added validation and error handling for new quote form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,7 +8,10 @@ class QuotesController < ApplicationController
   end
 
   def create
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong>. The quote needs to be between 3 and 140 characters'
+    end
     redirect_to root_path
   end
 
